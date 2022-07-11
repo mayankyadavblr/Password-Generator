a8b6336ffd8ba5da8b3e537074ce50b79e9055e4
mayankyadavblr <prehistoric0001@gmail.com>
2022-07-11 10:11:00 -0700

    improved output aesthetic

--- project.py
+++ project.py
@@ -7,11 +7,13 @@ while True:
     password_len=int(input('What length password do you need::'))
     no_of_passwords=int(input('how many passwords do you need::'))
     for i in range(0,no_of_passwords):
-        password=''
+        final_password=''
         print(i)
         #for j in range(0,password_len):
         password=random.choices(characters,k=password_len)
-        print('Here is your password::',password)
+        for j in password:
+            final_password+=j
+        print('Here is your password::',final_password)
     choice=input("Do you want to continue?\n(y/n)::")
     if choice=='n':
         break
