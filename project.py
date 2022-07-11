30cb78dfa2e2cea5e4af973dadbda49cc64ee91b
mayankyadavblr <prehistoric0001@gmail.com>
2022-07-11 09:59:12 -0700

    changed input screens

--- project.py
+++ project.py
@@ -3,8 +3,8 @@ import random
 characters='''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=_+[]\}{|;':",./<>?'''
 
 while True:
-    password_len=int(input(print('What length password do you need::')))
-    no_of_passwords=int(input(print('how many passwords do you need::')))
+    password_len=int(input('What length password do you need::'))
+    no_of_passwords=int(input('how many passwords do you need::'))
     for i in range(0,no_of_passwords):
         password=''
         print(i)
