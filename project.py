1fd82936abc0a588d027212ce88e5be0cf74dd43
mayankyadavblr <prehistoric0001@gmail.com>
2022-07-12 10:32:54 -0700

    made ver 1 of function

--- project.py
+++ project.py
@@ -1,6 +1,31 @@
 import random
 
 characters='''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=_+[]\}{|;':",./<>?'''
+characters_letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+characters_numbers='1234567890'
+characters_special='''!@#$%^&*()-=_+[]\}{|;':",./<>?'''
+
+def composition(total_length):
+    '''
+    This function determines how many of which
+    type of character make up the password
+    input: length of password
+    output: dict of numbers for the count of each of the
+    three types 
+    '''
+    dictionary={'letters':0,'numbers':0,'special':0}
+    character_type_choice=['letters','numbers','special']
+    for x in range(0,total_length-3):
+        choice1=random.choice(character_type_choice)
+        if choice1=='letters':
+            dictionary['letters']+=1
+        elif choice1=='numbers':
+            dictionary['numbers']+=1
+        elif choice1=='special':
+            dictionary['special']+=1
+    
+
+
 
 while True:
     
